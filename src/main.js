8d8c0d3635db607a9161eea67a34e189ba1c89eb
Irwanto Huang <irwantohuang@Irwantos-MacBook-Air.local>
2024-06-17 21:38:26 +0700

    fix deleted files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.css'
 import { FaIcon } from './plugins/fontAwesome'
-import { applyScrollReveal } from './config/srConfig'
+// import { applyScrollReveal } from './config/srConfig'
 // import scrollReveal from './plugins/scrollReveal'
 import { scrollReveal } from './plugins/scrollReveal'
 
