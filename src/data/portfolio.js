16fcb64ed38ee42221c2aeedf9352b6b9c4d6a32
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-18 16:00:50 +0700

    ADD Portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,56 +1,67 @@
-import WebLandingPage from '../assets/portfolio/web-landing-page.png'
-import WebPortfolio from '../assets/portfolio/web-portfolio.png'
-import WebLandingPage2 from '../assets/portfolio/web-landing-page-2.png'
-import Porto from '../assets/portfolio/porto.jpg'
+import Academia from '../assets/portfolio/academia.png'
+import NetflixClone from '../assets/portfolio/netflix-clone.png'
+import ExpandingCard from '../assets/portfolio/expanding-cards.png'
+import LoginForm from '../assets/portfolio/login-form.png'
+
+import MiracellSafety from '../assets/portfolio/miracell.png'
+import VueTailwindPorto from '../assets/portfolio/portfolio.png'
+import Interno from '../assets/portfolio/interno.png'
 
 export const portfolio = [
     {
-        title: 'Miracell Safety',
-        tools: ['HTML', 'CSS', 'Javascript'],
-        description: 'This is a landing page for Miracell Safety. an online safety shoe store. The goal was to create an attractive and responsive layout so visitors can easily explore the products offered.',
-        link: 'https://irwantohuang.github.io/miracell-web/',
-        image: WebLandingPage
-    },
-    {
-        title: 'Dummy Project - Portfolio',
+        title: 'Academia Connect',
         tools: ['Vue.js', 'TailwindCSS'],
-        description: 'The project serves as a platform for showcasing various works and projects, featuring a light and dark mode option for user preference. The emphasis is on presenting information clearly and engagingly, ensuring an optimal user experience.',
-        link: 'https://vuewithtailwind.netlify.app/',
-        image: WebPortfolio
+        description: 'Academia is an innovative educational platform designed to revolutionize learning experiences. It serves as a centralized hub for students, educators, and institutions alike, offering a dynamic ecosystem that fosters collaboration, knowledge-sharing, and personal growth.',
+        githubLink: 'https://github.com/irwantohuang/academia',
+        previewLink: 'https://academiaconnect.netlify.app/',
+        image: Academia,
     },
     {
-        title: 'Dummy Project - Interno',
+        title: 'Netflix Cloning',
         tools: ['HTML', 'CSS', 'Javascript'],
-        description: 'A dummy landing page designed for interno. an interior design concept. the focus was on creating an aesthetically pleasing and inviting interface to captivate visitors.',
-        link: '',
-        image: WebLandingPage2
-    },
-    {
-        title: 'Dummy Project - Interno',
-        tools: ['HTML', 'CSS', 'Javascript'],
-        description: 'A dummy landing page designed for interno. an interior design concept. the focus was on creating an aesthetically pleasing and inviting interface to captivate visitors.',
-        link: '',
-        image: WebLandingPage2
-    },
-    {
-        title: 'Dummy Project - Interno',
-        tools: ['HTML', 'CSS', 'Javascript'],
-        description: 'A dummy landing page designed for interno. an interior design concept. the focus was on creating an aesthetically pleasing and inviting interface to captivate visitors.',
-        link: '',
-        image: WebLandingPage2
+        description: 'Netflix Clone is a project aimed at recreating the popular streaming platform’s user interface. It allows users to browse through a library of movies and TV shows, watch trailers, and experience a seamless media streaming experience.',
+        githubLink: 'https://github.com/irwantohuang/netflix-clone.github.io',
+        previewLink: 'https://irwantohuang.github.io/netflix-clone.github.io/',
+        image: NetflixClone,
     },
     {
         title: 'Miracell Safety',
         tools: ['HTML', 'CSS', 'Javascript'],
         description: 'This is a landing page for Miracell Safety. an online safety shoe store. The goal was to create an attractive and responsive layout so visitors can easily explore the products offered.',
-        link: 'https://irwantohuang.github.io/miracell-web/',
-        image: WebLandingPage
+        githubLink: 'https://github.com/irwantohuang/miracell-web',
+        previewLink: 'https://irwantohuang.github.io/miracell-web/',
+        image: MiracellSafety,
     },
     {
         title: 'Dummy Project - Portfolio',
         tools: ['Vue.js', 'TailwindCSS'],
         description: 'The project serves as a platform for showcasing various works and projects, featuring a light and dark mode option for user preference. The emphasis is on presenting information clearly and engagingly, ensuring an optimal user experience.',
-        link: 'https://vuewithtailwind.netlify.app/',
-        image: WebPortfolio
+        githubLink: 'https://github.com/irwantohuang/portfolio-vue-tailwind',
+        previewLink: 'https://vuewithtailwind.netlify.app/',
+        image: VueTailwindPorto
+    },
+    {
+        title: 'Dummy Project - Interno',
+        tools: ['HTML', 'TailwindCSS', 'Javascript'],
+        description: 'A dummy landing page designed for interno. an interior design concept. the focus was on creating an aesthetically pleasing and inviting interface to captivate visitors.',
+        githubLink: 'https://github.com/irwantohuang/interno',
+        previewLink: 'https://internopages.netlify.app/',
+        image: Interno
+    },
+    {
+        title: 'Expanding Cards',
+        tools: ['HTML', 'CSS', 'Javascript'],
+        description: 'A minimalist project showcasing interactive cards that expand and reveal content upon clicking. Built with HTML, CSS for styling, and JavaScript for functionality.',
+        githubLink: 'https://github.com/irwantohuang/expanding-cards',
+        previewLink: 'https://irwantohuang.github.io/expanding-cards/',
+        image: ExpandingCard
+    },
+    {
+        title: 'Login Form',
+        tools: ['HTML', 'CSS', 'Javascript'],
+        description: 'A basic login form project demonstrating front-end development skills with HTML for structure, CSS for styling, and JavaScript for form validation and interactivity.',
+        githubLink: 'https://github.com/irwantohuang/login-form',
+        previewLink: 'https://irwantohuang.github.io/login-form/',
+        image: LoginForm
     },
 ]
